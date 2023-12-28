2b1e86b80d97f849e894f44ab8766f72bf40a076
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-12-28 10:00:00 +0100

    assignment

---
+++ returnShortestWord.js
@@ -0,0 +1,19 @@
+/* 5. write a function that takes an array of strings and returns the shortest word. */
+
+function returnShortestWord(words) {
+    let shortest = words[0];
+    
+    for (let word of words) {
+        if (word.length < shortest.length) {
+            shortest = word;
+        }
+    }
+    
+    return shortest;
+}
+
+const words = ["apple", "banana", "orange", "pear"];
+console.log(returnShortestWord(words)); 
+
+
+
